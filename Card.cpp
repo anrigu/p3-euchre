8cbb23ef23531eb1aa4a2b1802b46ead6b5c453e
anrigu <anrigu@umich.edu>
2022-02-14 18:02:52 -0500

    Card update

--- Card.cpp
+++ Card.cpp
@@ -6,25 +6,6 @@
 #include "Card.h"
 // add any necessary #include or using directives here
 
-// rank and suit names -- do not remove these
-constexpr const char* const Card::RANK_TWO;
-constexpr const char* const Card::RANK_THREE;
-constexpr const char* const Card::RANK_FOUR;
-constexpr const char* const Card::RANK_FIVE;
-constexpr const char* const Card::RANK_SIX;
-constexpr const char* const Card::RANK_SEVEN;
-constexpr const char* const Card::RANK_EIGHT;
-constexpr const char* const Card::RANK_NINE;
-constexpr const char* const Card::RANK_TEN;
-constexpr const char* const Card::RANK_JACK;
-constexpr const char* const Card::RANK_QUEEN;
-constexpr const char* const Card::RANK_KING;
-constexpr const char* const Card::RANK_ACE;
-constexpr const char* const Card::SUIT_SPADES;
-constexpr const char* const Card::SUIT_HEARTS;
-constexpr const char* const Card::SUIT_CLUBS;
-constexpr const char* const Card::SUIT_DIAMONDS;
-
 //EFFECTS Initializes Card to the Two of Spades
 Card::Card()
     : rank(RANK_TWO), suit(SUIT_SPADES) {};
